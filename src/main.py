0cce7a4404ebb404e78ad82e31d101f150f16738
Pedro Luis <pedroluisnr22@gmail.com>
2025-04-25 22:26:50 -0500

    feat: :sparkles: add first survey view

--- src/main.py
+++ src/main.py
@@ -1,22 +1,59 @@
 import flet as ft
 
+from src.survey import Survey, SurveyStep
+
 
 def main(page: ft.Page):
-    counter = ft.Text("0", size=50, data=0)
+    survey = Survey(
+        name="Survey 1",
+        steps=[
+            SurveyStep(
+                question="What is your name?",
+                field_type="text"
+            ),
+            SurveyStep(
+                question="What is your lastname?",
+                field_type="text"
+            )
+        ]
+    )
+    survey_step_index = 0
+    survey_step = survey.steps[survey_step_index]
 
-    def increment_click(e):
-        counter.data += 1
-        counter.value = str(counter.data)
-        counter.update()
+    survey_step_text = ft.Text(survey_step.question)
+    survey_step_field = ft.TextField(value="")
+
+    def next_step_click(_):
+        nonlocal survey_step_index
+        nonlocal survey_step
+
+        if survey_step_index < len(survey.steps) - 1:
+            survey_step_index = survey_step_index + 1
+            survey_step = survey.steps[survey_step_index]
+
+            survey_step_text.value = survey_step.question
+            survey_step_text.update()
 
     page.floating_action_button = ft.FloatingActionButton(
-        icon=ft.Icons.ADD, on_click=increment_click
+        icon=ft.Icons.ARROW_RIGHT, on_click=next_step_click
     )
     page.add(
         ft.SafeArea(
             ft.Container(
-                counter,
+                ft.Column(
+                    controls=[
+                        ft.Text(survey.name),
+                        ft.VerticalDivider(width=12),
+                        ft.Column(
+                            controls=[
+                                survey_step_text,
+                                survey_step_field
+                            ]
+                        )
+                    ]
+                ),
                 alignment=ft.alignment.center,
+                padding=ft.padding.all(20)
             ),
             expand=True,
         )
